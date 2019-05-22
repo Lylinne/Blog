a1bd6da0228c1d88d42fe0947342ca60e47f5619
Lylinne <nikkinoaya@gmail.com>
2019-05-22 14:31:11 +0200

    pas fini

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var carouselItem = [
+				[ "Kenya",
+					"https://cdn.pixabay.com/photo/2017/08/27/07/47/giraffes-2685352_960_720.jpg",
+					"Le Kenya offre au voyageur un choix unique d'options, avec une incroyable diversité de paysages, de cultures, de faune, de flore, et d'activités. Les safaris constituent bien sûr la principale attraction du Kenya, qui réunit quelques-unes des réserves les plus préservées au monde. "
+					],
+					["Bali",
+					  "https://cdn.pixabay.com/photo/2016/11/14/02/47/agriculture-1822442_960_720.jpg",
+					  "Bali est une île indonésienne célèbre pour ses montagnes volcaniques boisées, ses rizières, ses plages et ses récifs coralliens. L’île abrite des sites religieux, comme le temple d’Uluwatu construit au sommet d’une falaise à pic"
+					],
+					["Jordani",
+						"https://cdn.pixabay.com/photo/2013/09/13/12/24/jordan-181912_960_720.jpg",
+						"Description La Jordanie est un pays arabe situé sur la rive orientale du Jourdain. Il se compose de monuments anciens, de réserves naturelles et de stations balnéaires. Le pays abrite notamment le célèbre site archéologique de Pétra, la capitale nabatéenne fondée autour de 300 ans av. J-C. "
+					]
+
+					];
